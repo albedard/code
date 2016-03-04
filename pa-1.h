3aefd64c9d969005949d3aef9144ae567cd18993
Amanda Bedard <albedard@my.uri.edu>
2016-03-04 14:42:56 -0500

    commit

---
+++ pa-1.h
@@ -0,0 +1,31 @@
+#ifndef pa_1_H_
+#define pa_1_H_
+#include <stdbool.h>
+
+// The declaration of all the functios in the header
+//
+bool f_armstrong(int n);
+
+bool f_brackets(char* e);
+
+bool f_perfect(int x);
+
+int f_rotate(int n, int r);
+
+int f_str_search(char* pattern, char* text);
+
+int f_collatz_count(int n);
+
+float f_array(int n);
+
+void f_strings(char* s1, char* s2, char* s3);
+
+void f_sort(int x, int y, int z, int* list);
+
+int* f_cubes_sum(int n);
+
+int power(int n, int p);
+
+int CharArrLen(char* arr);
+
+#endif //pa-1_H_
